d15b8da9b6b16c83dd47c9bf003ebb2fd1cd2706
Peter Kotik <thatotherpeter@gmail.com>
2015-11-12 14:59:59 -0800

    added tables.py

---
+++ models/tables.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+db.define_table('userinfo',
+                Field('screenname', requires=IS_NOT_EMPTY()),
+                Field('fluent', 'list:string'),
+                Field('learn', 'list:string')
+                )
+
+db.define_table('messages',
+                Field('userfrom', 'reference userinfo'),
+                Field('userto', 'reference userinfo'),
+                Field('message'),
+                Field('time', 'datetime', default = datetime.utcnow())
+                )
